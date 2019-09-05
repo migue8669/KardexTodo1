f64f20389fb320e2dec7a600cd3cb334df4434ce
miguel angel ramirez escobar <miguelramirez8669@gmail.com>
2019-09-04 20:50:28 -0500

    Nuevo back con maven, se elimina hecho con gradle y sin servlets

--- back-kardextodo1/src/main/java/com/example/kardex/Dominio/Repositiorio/ProductosRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.example.kardex.Dominio.Repositiorio;
-
-
-import com.example.kardex.Dominio.Modelo.Productos;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductosRepositorio extends MongoRepository<Productos, String> {}
